757e553d3c7ac654b2d24855748aeadd4f12b1ab
tdarlic <tomislav.darlic@gmail.com>
2024-11-28 17:48:11 +0100

    Adding first test files from CH32V003FUN
    blink test is copied and sucessfully compiled and flashed

---
+++ firmware/src/blink.c
@@ -0,0 +1,37 @@
+#include "ch32v003fun.h"
+#include <stdio.h>
+
+// use defines to make more meaningful names for our GPIO pins
+#define PIN_1 PD0
+#define PIN_K PD4
+#define PIN_BOB PD6
+#define PIN_KEVIN PC0
+
+#define DELAYS 150
+
+int main()
+{
+	SystemInit();
+
+	// Enable GPIOs
+	funGpioInitAll();
+	
+	funPinMode( PIN_1,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_K,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_BOB,   GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PIN_KEVIN, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+
+	while(1)
+	{
+		// funDigitalWrite( PIN_1,     FUN_HIGH );
+		// funDigitalWrite( PIN_K,     FUN_HIGH );
+		funDigitalWrite( PIN_BOB,   FUN_HIGH );
+		// funDigitalWrite( PIN_KEVIN, FUN_HIGH );
+		Delay_Ms( DELAYS );
+		// funDigitalWrite( PIN_1,     FUN_LOW );
+		// funDigitalWrite( PIN_K,     FUN_LOW );
+		funDigitalWrite( PIN_BOB,   FUN_LOW );
+		// funDigitalWrite( PIN_KEVIN, FUN_LOW );
+		Delay_Ms( DELAYS );
+	}
+}
