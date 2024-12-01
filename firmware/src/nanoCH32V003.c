da672668dca49477638870f3ace973b7cb63b0da
tdarlic <tomislav.darlic@gmail.com>
2024-12-01 16:52:31 +0100

    Corrected the clock with FUNCONF_SYSTICK_USE_HCLK

--- firmware/src/nanoCH32V003.c
+++ firmware/src/nanoCH32V003.c
@@ -2,18 +2,43 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// Number of ticks elapsed per millisecond (48,000 when using 48MHz Clock)
+#define SYSTICK_ONE_MILLISECOND ((uint32_t)FUNCONF_SYSTEM_CORE_CLOCK / 1000)
+// Number of ticks elapsed per microsecond (48 when using 48MHz Clock)
+#define SYSTICK_ONE_MICROSECOND ((uint32_t)FUNCONF_SYSTEM_CORE_CLOCK / 1000000)
+
+// Simple macro functions to give a arduino-like functions to call
+// millis() reads the incremented systick variable
+// micros() reads the raw SysTick Count, and divides it by the number of 
+// ticks per microsecond ( WARN: Wraps every 90 seconds!)
+#define millis() (systick_millis)
+#define micros() (SysTick->CNT / SYSTICK_ONE_MICROSECOND)
+
+// Incremented in the SysTick IRQ - in this example once per millisecond
+volatile uint32_t systick_millis;
+
 // This is only for devboard
 uint16_t adc1, adc2, adc3, adc4 = 0;
 
 #define PIN_LED PD6
 
-#define DELAYS 50
+#define DELAYS 500
 #define MINLED 2
-#define MAXLED 30
-#define MAXLEDPWM 1600
+#define MAXLED 1500
+#define LEDSPEED 70
 
-uint16_t pwmval(){
-	static uint8_t cnt = MINLED;
+#define LED_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 6), high);
+#define LED_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 6), low);
+
+/**
+* Returns value for PWM
+* First it increases th value of counter 
+* And than it cubes it
+* Starts decreasing in case that PWM value is larger than MAXLEDPWM
+* Starts increasing in case that counter is less than MINLED
+*/
+inline static uint16_t getpwmval(void){
+	static uint16_t cnt = MINLED;
 	static int8_t dir = 1;
 	uint32_t out = 0;
 	
@@ -21,19 +46,93 @@ uint16_t pwmval(){
 	out = cnt * cnt * cnt;
 	// printf("out: %d\n", out);
 
-	if (out > MAXLEDPWM){
+	if (out > MAXLED){
 		dir = -1;	
 	}
 	if (cnt < MINLED){
 		dir = 1;
 	}
-
 	return out;
+}
+
+/*
+ * Initialises the SysTick to trigger an IRQ with auto-reload, using HCLK/1 as
+ * its clock source
+ */
+void systick_init(void)
+{
+	// Reset any pre-existing configuration
+	SysTick->CTLR = 0x0000;
+	
+	// Set the compare register to trigger once per millisecond
+	SysTick->CMP = SYSTICK_ONE_MILLISECOND - 1;
+
+	// Reset the Count Register, and the global millis counter to 0
+	SysTick->CNT = 0x00000000;
+	systick_millis = 0x00000000;
+	
+	// Set the SysTick Configuration
+	// NOTE: By not setting SYSTICK_CTLR_STRE, we maintain compatibility with
+	// busywait delay funtions used by ch32v003_fun.
+	SysTick->CTLR |= SYSTICK_CTLR_STE   |  // Enable Counter
+	                 SYSTICK_CTLR_STIE  |  // Enable Interrupts
+	                 SYSTICK_CTLR_STCLK ;  // Set Clock Source to HCLK/1
+	//
+		
+	
+	// Enable the SysTick IRQ
+	NVIC_EnableIRQ(SysTicK_IRQn);
+}
+
+/*
+ * SysTick ISR - must be lightweight to prevent the CPU from bogging down.
+ * Increments Compare Register and systick_millis when triggered (every 1ms)
+ * NOTE: the `__attribute__((interrupt))` attribute is very important
+ */
+void SysTick_Handler(void) __attribute__((interrupt));
+void SysTick_Handler(void)
+{
+	// Increment the Compare Register for the next trigger
+	// If more than this number of ticks elapse before the trigger is reset,
+	// you may miss your next interrupt trigger
+	// (Make sure the IQR is lightweight and CMP value is reasonable)
+	SysTick->CMP += SYSTICK_ONE_MILLISECOND;
+
+	// Clear the trigger state for the next IRQ
+	SysTick->SR = 0x00000000;
+
+	// Increment the milliseconds count
+	systick_millis++;
+	if (!(systick_millis % LEDSPEED)){
+		GPIO_tim2_analogWrite(3, getpwmval());
+	 }
+}
+
+void printRCC(void){
+	uint32_t regtemp, trim;
+	regtemp = RCC->CTLR;
+	printf("Printing RCC - clock control register\r\n");
+	printf("RCC->CTLR: 0x%08lX:\r\n", (unsigned long) regtemp);
+	if (regtemp & RCC_PLLRDY) printf("RCC_PLLRDY \r\n");
+	if (regtemp & RCC_PLLON)  printf("RCC_PLLON  \r\n");
+	if (regtemp & RCC_CSSON)  printf("RCC_CSSON  \r\n");
+	if (regtemp & RCC_HSEBYP) printf("RCC_HSEBYP \r\n");
+	if (regtemp & RCC_HSERDY) printf("RCC_HSERDY \r\n");
+	if (regtemp & RCC_HSEON)  printf("RCC_HSEON  \r\n");
+	if (regtemp & RCC_HSIRDY) printf("RCC_HSIRDY \r\n");
+	if (regtemp & RCC_HSION)  printf("RCC_HSION  \r\n");
+	printf("\r\n");
+	printf("HSICAL: 0x%02lX\r\n", (unsigned long) (regtemp & RCC_HSICAL) >> 8);
+	trim = (regtemp & RCC_HSITRIM) >> 3;
+	printf("HSITRIM: 0x%02lX\r\n", (unsigned long) trim);
 }
 
 int main()
 {
 	SystemInit();
+	systick_init();
+
+	printRCC();
 
 	GPIO_port_enable(GPIO_port_C);
 	GPIO_port_enable(GPIO_port_D);
@@ -60,16 +159,14 @@ int main()
 	printf("ADC test\n");
 	while(1)
 	{
-		GPIO_tim2_analogWrite(3, pwmval());
-		// GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 6), high);
-		// Delay_Ms( DELAYS );
-		// GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 6), low);
+		// uint32_t targend = SysTick->CNT + n;
+		// while( ((int32_t)( SysTick->CNT - targend )) < 0 );
 		Delay_Ms( DELAYS );
 		// Read all 4 adc
 		adc1 = GPIO_analogRead(GPIO_Ain4_D3);
 		adc2 = GPIO_analogRead(GPIO_Ain7_D4);
 		adc3 = GPIO_analogRead(GPIO_Ain2_C4);
 		adc4 = GPIO_analogRead(GPIO_Ain3_D2);
-		// printf("ADC: %d | %d | %d | %d\n", adc1, adc2, adc3, adc4);
+		printf("[%d] ADC: %d | %d | %d | %d\n", millis(), adc1, adc2, adc3, adc4);
 	}
 }
