40d32638c6f8cea95a88001e91b1107659098498
tdarlic <tomislav.darlic@gmail.com>
2024-12-12 21:43:11 +0100

    Add separte folders for main and devboard code
    Add comments to hardware

---
+++ firmware/devboard/ch32v003fun.mk
@@ -0,0 +1,123 @@
+PREFIX?=riscv64-unknown-elf
+OUTDIR ?= build
+NEWLIB?=/usr/include/newlib
+
+TARGET_MCU?=CH32V003
+TARGET_EXT?=c
+
+CH32V003FUN?=$(dir $(lastword $(MAKEFILE_LIST)))
+MINICHLINK?=$(CH32V003FUN)/../minichlink
+
+WRITE_SECTION?=flash
+SYSTEM_C?=$(CH32V003FUN)/ch32v003fun.c
+
+ifeq ($(DEBUG),1)
+	EXTRA_CFLAGS+=-DFUNCONF_DEBUG=1
+endif
+
+CFLAGS?=-g -Os -flto -ffunction-sections -fdata-sections -fmessage-length=0 -msmall-data-limit=8
+LDFLAGS+=-Wl,--print-memory-usage
+
+ifeq ($(TARGET_MCU),CH32V003)
+	CFLAGS_ARCH+=-march=rv32ec -mabi=ilp32e -DCH32V003=1
+	GENERATED_LD_FILE?=$(CH32V003FUN)/generated_ch32v003.ld
+	TARGET_MCU_LD:=0
+	LINKER_SCRIPT?=$(GENERATED_LD_FILE)
+	LDFLAGS+=-L$(CH32V003FUN)/../misc -lgcc
+else
+	MCU_PACKAGE?=1
+
+	ifeq ($(findstring CH32V10,$(TARGET_MCU)),CH32V10) # CH32V103
+		TARGET_MCU_PACKAGE?=CH32V103R8T6
+		CFLAGS_ARCH+=	-march=rv32imac \
+			-mabi=ilp32 \
+			-DCH32V10x=1
+
+		# MCU Flash/RAM split
+		ifeq ($(findstring R8, $(TARGET_MCU_PACKAGE)), R8)
+			MCU_PACKAGE:=1
+		else ifeq ($(findstring C8, $(TARGET_MCU_PACKAGE)), C8)
+			MCU_PACKAGE:=1
+		else ifeq ($(findstring C6, $(TARGET_MCU_PACKAGE)), C6)
+			MCU_PACKAGE:=2
+		endif
+
+		TARGET_MCU_LD:=1
+	else
+		$(error Unknown MCU $(TARGET_MCU))
+	endif
+
+	LDFLAGS+=-lgcc
+	GENERATED_LD_FILE:=$(CH32V003FUN)/generated_$(TARGET_MCU_PACKAGE)_$(TARGET_MCU_MEMORY_SPLIT).ld
+	LINKER_SCRIPT:=$(GENERATED_LD_FILE)
+endif
+
+CFLAGS+= \
+	$(CFLAGS_ARCH) -static-libgcc \
+	-I$(NEWLIB) \
+	-I$(CH32V003FUN)/../extralibs \
+	-I$(CH32V003FUN) \
+	-nostdlib \
+	-I. -Wall $(EXTRA_CFLAGS)
+
+LDFLAGS+=-T $(LINKER_SCRIPT) -Wl,--gc-sections
+FILES_TO_COMPILE:=$(SYSTEM_C) $(TARGET).$(TARGET_EXT) $(ADDITIONAL_C_FILES) 
+
+$(OUTDIR)/$(TARGET).bin : $(OUTDIR)/$(TARGET).elf
+	mkdir -p $(OUTDIR)
+	$(PREFIX)-objdump -S $^ > $(OUTDIR)/$(TARGET).lst
+	$(PREFIX)-objdump -t $^ > $(OUTDIR)/$(TARGET).map
+	$(PREFIX)-objcopy -O binary $< $(OUTDIR)/$(TARGET).bin
+	$(PREFIX)-objcopy -O ihex $< $(OUTDIR)/$(TARGET).hex
+
+ifeq ($(OS),Windows_NT)
+closechlink :
+	-taskkill /F /IM minichlink.exe /T
+else
+closechlink :
+	-killall minichlink
+endif
+
+terminal : monitor
+
+monitor :
+	$(MINICHLINK)/minichlink -T
+
+unbrick :
+	$(MINICHLINK)/minichlink -u
+
+gdbserver : 
+	-$(MINICHLINK)/minichlink -baG
+
+clangd :
+	make clean
+	bear -- make build
+	@echo "CompileFlags:" > .clangd
+	@echo "  Remove: [-march=*, -mabi=*]" >> .clangd
+
+clangd_clean :
+	rm -f compile_commands.json .clangd
+	rm -rf .cache
+
+FLASH_COMMAND?=$(MINICHLINK)/minichlink -w $< $(WRITE_SECTION) -b
+
+.PHONY : $(GENERATED_LD_FILE)
+$(GENERATED_LD_FILE) :
+	$(PREFIX)-gcc -E -P -x c -DTARGET_MCU=$(TARGET_MCU) -DMCU_PACKAGE=$(MCU_PACKAGE) -DTARGET_MCU_LD=$(TARGET_MCU_LD) -DTARGET_MCU_MEMORY_SPLIT=$(TARGET_MCU_MEMORY_SPLIT) $(CH32V003FUN)/ch32v003fun.ld > $(GENERATED_LD_FILE)
+
+$(OUTDIR)/$(TARGET).elf : $(FILES_TO_COMPILE) $(LINKER_SCRIPT) $(EXTRA_ELF_DEPENDENCIES)
+	$(PREFIX)-gcc -o $@ $(FILES_TO_COMPILE) $(CFLAGS) $(LDFLAGS)
+
+# Rule for independently building ch32v003fun.o indirectly, instead of recompiling it from source every time.
+# Not used in the default 003fun toolchain, but used in more sophisticated toolchains.
+ch32v003fun.o : $(SYSTEM_C)
+	$(PREFIX)-gcc -c -o $@ $(SYSTEM_C) $(CFLAGS)
+
+cv_flash : $(OUTDIR)/$(TARGET).bin
+	make -C $(MINICHLINK) all
+	$(FLASH_COMMAND)
+
+cv_clean :
+	rm -rf $(OUTDIR)/$(TARGET).elf $(OUTDIR)/$(TARGET).bin $(OUTDIR)/$(TARGET).hex $(OUTDIR)/$(TARGET).lst $(OUTDIR)/$(TARGET).map $(OUTDIR)/$(TARGET).hex $(OUTDIR)/$(GENERATED_LD_FILE) || true
+
+build : $(OUTDIR)/$(TARGET).bin
---
+++ firmware/src/main.c
@@ -0,0 +1,177 @@
+#include "ch32v003_GPIO_branchless.h"
+#include <stdint.h>
+#include <stdio.h>
+
+// Number of ticks elapsed per millisecond (48,000 when using 48MHz Clock)
+#define SYSTICK_ONE_MILLISECOND ((uint32_t)FUNCONF_SYSTEM_CORE_CLOCK / 1000)
+// Number of ticks elapsed per microsecond (48 when using 48MHz Clock)
+#define SYSTICK_ONE_MICROSECOND ((uint32_t)FUNCONF_SYSTEM_CORE_CLOCK / 1000000)
+
+// Simple macro functions to give a arduino-like functions to call
+// millis() reads the incremented systick variable
+// micros() reads the raw SysTick Count, and divides it by the number of 
+// ticks per microsecond ( WARN: Wraps every 90 seconds!)
+#define millis() (systick_millis)
+#define micros() (SysTick->CNT / SYSTICK_ONE_MICROSECOND)
+
+// Incremented in the SysTick IRQ - in this example once per millisecond
+volatile uint32_t systick_millis;
+
+// This is only for devboard
+uint16_t adc1, adc2, adc3, adc4 = 0;
+
+#define PIN_LED PD6
+
+#define DELAYS 1000
+#define MINLED 2
+#define MAXLED 2000
+#define LEDSPEED 200
+
+#define LED_MAIN_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 2), high)
+#define LED_MAIN_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 2), low)
+
+#define LED1_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 7), high)
+#define LED1_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_D, 7), low)
+
+#define LED2_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 1), high)
+#define LED2_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 1), low)
+
+#define LED3_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 0), high)
+#define LED3_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_A, 0), low)
+
+#define LED4_ON GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_C, 0), high)
+#define LED4_OFF GPIO_digitalWrite(GPIOv_from_PORT_PIN(GPIO_port_C, 0), low)
+
+/**
+* Returns value for PWM
+* First it increases th value of counter 
+* And than it cubes it
+* Starts decreasing in case that PWM value is larger than MAXLEDPWM
+* Starts increasing in case that counter is less than MINLED
+*/
+inline static uint16_t getpwmval(void){
+	static uint16_t cnt = MINLED;
+	static int8_t dir = 1;
+	uint32_t out = 0;
+	
+	cnt = cnt + dir;
+	out = cnt * cnt * cnt;
+	// printf("out: %d\n", out);
+
+	if (out > MAXLED){
+		dir = -1;	
+	}
+	if (cnt < MINLED){
+		dir = 1;
+	}
+	return out;
+}
+
+/*
+ * Initialises the SysTick to trigger an IRQ with auto-reload, using HCLK/1 as
+ * its clock source
+ */
+void systick_init(void)
+{
+	// Reset any pre-existing configuration
+	SysTick->CTLR = 0x0000;
+	
+	// Set the compare register to trigger once per millisecond
+	SysTick->CMP = SYSTICK_ONE_MILLISECOND - 1;
+
+	// Reset the Count Register, and the global millis counter to 0
+	SysTick->CNT = 0x00000000;
+	systick_millis = 0x00000000;
+	
+	// Set the SysTick Configuration
+	// NOTE: By not setting SYSTICK_CTLR_STRE, we maintain compatibility with
+	// busywait delay funtions used by ch32v003_fun.
+	SysTick->CTLR |= SYSTICK_CTLR_STE   |  // Enable Counter
+	                 SYSTICK_CTLR_STIE  |  // Enable Interrupts
+	                 SYSTICK_CTLR_STCLK ;  // Set Clock Source to HCLK/1
+	//
+		
+	
+	// Enable the SysTick IRQ
+	NVIC_EnableIRQ(SysTicK_IRQn);
+}
+
+/*
+ * SysTick ISR - must be lightweight to prevent the CPU from bogging down.
+ * Increments Compare Register and systick_millis when triggered (every 1ms)
+ * NOTE: the `__attribute__((interrupt))` attribute is very important
+ */
+void SysTick_Handler(void) __attribute__((interrupt));
+void SysTick_Handler(void)
+{
+	// Increment the Compare Register for the next trigger
+	// If more than this number of ticks elapse before the trigger is reset,
+	// you may miss your next interrupt trigger
+	// (Make sure the IQR is lightweight and CMP value is reasonable)
+	SysTick->CMP += SYSTICK_ONE_MILLISECOND;
+
+	// Clear the trigger state for the next IRQ
+	SysTick->SR = 0x00000000;
+
+	// Increment the milliseconds count
+	systick_millis++;
+	if (!(systick_millis % LEDSPEED)){
+		GPIO_tim2_analogWrite(2, getpwmval());
+	 }
+}
+
+int main()
+{
+	SystemInit();
+	systick_init();
+
+	GPIO_tim1_init();
+	NVIC_EnableIRQ(TIM1_UP_IRQn);
+
+	GPIO_port_enable(GPIO_port_C);
+	GPIO_port_enable(GPIO_port_D);
+	GPIO_port_enable(GPIO_port_A);
+	// MAIN LED
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_A, 2), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	// LED1
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_D, 7), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	// LED2
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_A, 1), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	// LED3
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_A, 0), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	// LED4
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_C, 0), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+
+	// GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_D, 7), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	// GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_C, 0), GPIO_pinMode_O_pushPull, GPIO_Speed_2MHz);
+	
+	// GPIO D3 analog in - PD3 - A4 - ADC1
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_D, 3), GPIO_pinMode_I_analog, GPIO_Speed_In);
+	// GPIO D4 analog in - PD4 - A7 - ADC2
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_D, 4), GPIO_pinMode_I_analog, GPIO_Speed_In);
+	// GPIO C4 analog in - PC4 - A2 - ADC3
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_C, 4), GPIO_pinMode_I_analog, GPIO_Speed_In);
+	// GPIO D2 analog in - PD2 - A3 - ADC4
+	GPIO_pinMode(GPIOv_from_PORT_PIN(GPIO_port_D, 2), GPIO_pinMode_I_analog, GPIO_Speed_In);
+
+	GPIO_ADC_set_sampletimes_all(GPIO_ADC_sampletime);
+	GPIO_ADCinit();
+
+	printf("ADC test\n");
+	while(1)
+	{
+		LED_MAIN_ON;
+		// Read all 4 adc
+		adc1 = GPIO_analogRead(GPIO_Ain4_D3);
+		adc2 = GPIO_analogRead(GPIO_Ain7_D4);
+		adc3 = GPIO_analogRead(GPIO_Ain2_C4);
+		adc4 = GPIO_analogRead(GPIO_Ain3_D2);
+		// printf("TIM1_CNT / TIM1_RPTCNT: %d / %d\r\n", TIM1->CNT, TIM1->RPTCR);
+		printf("[%d] ADC: %d | %d | %d | %d\n", millis(), adc1, adc2, adc3, adc4);
+		(adc1 < 500) ? (LED1_ON) : (LED1_OFF);
+		(adc2 < 500) ? (LED2_ON) : (LED2_OFF);
+		(adc3 < 500) ? (LED3_ON) : (LED3_OFF);
+		(adc4 < 500) ? (LED4_ON) : (LED4_OFF);
+		Delay_Ms( DELAYS );
+	}
+}
